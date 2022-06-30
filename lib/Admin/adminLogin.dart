64a72f33c711be98436c5072c8ea65f56d0b2929
Dhiraj Pakhrin <dhirajpakhrin95@gmail.com>
2022-06-30 14:14:14 +0545

    init

--- lib/Admin/adminLogin.dart
+++ lib/Admin/adminLogin.dart
@@ -5,27 +5,179 @@ import 'package:e_shop/Widgets/customTextField.dart';
 import 'package:e_shop/DialogBox/errorDialog.dart';
 import 'package:flutter/material.dart';
 
-
-
-
 class AdminSignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: new LinearGradient(
+              colors: [Colors.pink, Colors.lightGreenAccent],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        title: Text(
+          "e-Shop",
+          style: TextStyle(
+              fontSize: 55.0, color: Colors.white, fontFamily: "Signatra"),
+        ),
+        centerTitle: true,
+      ),
+      body: AdminSignInScreen(),
+    );
   }
 }
-
 
 class AdminSignInScreen extends StatefulWidget {
   @override
   _AdminSignInScreenState createState() => _AdminSignInScreenState();
 }
 
-class _AdminSignInScreenState extends State<AdminSignInScreen>
-{
+class _AdminSignInScreenState extends State<AdminSignInScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _adminIDTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: new LinearGradient(
+            colors: [Colors.pink, Colors.lightGreenAccent],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Image.asset(
+                "images/admin.png",
+                height: 240.0,
+                width: 240.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(0.0),
+              child: Text(
+                "Admin",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  CustomTextField(
+                    controller: _adminIDTextEditingController,
+                    data: Icons.person,
+                    hintText: "id",
+                    isObsecure: false,
+                  ),
+                  CustomTextField(
+                    controller: _passwordTextEditingController,
+                    data: Icons.person,
+                    hintText: "Password",
+                    isObsecure: true,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            RaisedButton(
+              onPressed: () {
+                _adminIDTextEditingController.text.isNotEmpty &&
+                        _passwordTextEditingController.text.isNotEmpty
+                    ? loginAdmin()
+                    : showDialog(
+                        context: context,
+                        builder: (c) {
+                          return ErrorAlertDialog(
+                            message: "Please write email and password.",
+                          );
+                        });
+              },
+              child: Text(
+                "Login",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Container(
+              height: 4.0,
+              width: MediaQuery.of(context).size.width * 0.8,
+              color: Colors.pink,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            FlatButton.icon(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AuthenticScreen())),
+              icon: (Icon(
+                Icons.nature_people,
+                color: Colors.pink,
+              )),
+              label: Text(
+                "I'm not Admin",
+                style: TextStyle(
+                  color: Colors.pink,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-    return SingleChildScrollView();
+  loginAdmin() {
+    Firestore.instance.collection('admins').getDocuments().then((snapshot) {
+      snapshot.documents.forEach((result) {
+        if (result.data['id'] != _adminIDTextEditingController.text.trim()) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text('your id is not correct.'),
+          ));
+        } else if (result.data['password'] !=
+            _passwordTextEditingController.text.trim()) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text('your password is not correct.'),
+          ));
+        } else {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text('Welcome Dear Admin, ' + result.data['name']),
+          ));
+          setState(() {
+            _adminIDTextEditingController.text = "";
+            _passwordTextEditingController.text = "";
+          });
+          Route route = MaterialPageRoute(builder: (c) => UploadPage());
+          Navigator.pushReplacement(context, route);
+        }
+      });
+    });
   }
 }
--- lib/Authentication/register.dart
+++ lib/Authentication/register.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_shop/Widgets/customTextField.dart';
 import 'package:e_shop/DialogBox/errorDialog.dart';
 import 'package:e_shop/DialogBox/loadingDialog.dart';
@@ -9,19 +10,219 @@ import 'package:image_picker/image_picker.dart';
 import '../Store/storehome.dart';
 import 'package:e_shop/Config/config.dart';
 
-
-
 class Register extends StatefulWidget {
   @override
   _RegisterState createState() => _RegisterState();
 }
 
+class _RegisterState extends State<Register> {
+  final TextEditingController _nameTextEditingController =
+      TextEditingController();
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+  final TextEditingController _cPasswordTextEditingController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String userImageUrl = "";
+  File _imageFile;
 
-
-class _RegisterState extends State<Register>
-{
   @override
   Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width,
+        _screenHeight = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            SizedBox(
+              height: 10.0,
+            ),
+            InkWell(
+              onTap: _selectAndPickImage,
+              child: CircleAvatar(
+                radius: _screenWidth * 0.15,
+                backgroundColor: Colors.white,
+                backgroundImage:
+                    _imageFile == null ? null : FileImage(_imageFile),
+                child: _imageFile == null
+                    ? Icon(
+                        Icons.add_a_photo,
+                        size: _screenWidth * 0.15,
+                        color: Colors.grey,
+                      )
+                    : null,
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  CustomTextField(
+                    controller: _nameTextEditingController,
+                    data: Icons.person,
+                    hintText: "Name",
+                    isObsecure: false,
+                  ),
+                  CustomTextField(
+                    controller: _emailTextEditingController,
+                    data: Icons.email,
+                    hintText: "Email",
+                    isObsecure: false,
+                  ),
+                  CustomTextField(
+                    controller: _passwordTextEditingController,
+                    data: Icons.person,
+                    hintText: "Password",
+                    isObsecure: true,
+                  ),
+                  CustomTextField(
+                    controller: _cPasswordTextEditingController,
+                    data: Icons.person,
+                    hintText: "Confirm Password",
+                    isObsecure: true,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      uploadAndSaveImage();
+                    },
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                    height: 4.0,
+                    width: _screenWidth * 0.8,
+                    color: Colors.pink,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _selectAndPickImage() async {
+    _imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+  }
+
+  Future<void> uploadAndSaveImage() async {
+    if (_imageFile == null) {
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorAlertDialog(
+              message: "Please select an image file",
+            );
+          });
+    } else {
+      _passwordTextEditingController.text ==
+              _cPasswordTextEditingController.text
+          ? _emailTextEditingController.text.isNotEmpty &&
+                  _passwordTextEditingController.text.isNotEmpty &&
+                  _cPasswordTextEditingController.text.isNotEmpty &&
+                  _nameTextEditingController.text.isNotEmpty
+              ? uploadToStorage()
+              : displayDialog("Please fill up the complete form...")
+          : displayDialog("Password do not match. ");
+    }
+  }
+
+  displayDialog(String msg) {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return ErrorAlertDialog(message: msg);
+        });
+  }
+
+  uploadToStorage() async {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return LoadingAlertDialog(
+            message: "'Registering, Please wait......'",
+          );
+        });
+
+    String imageFileName = DateTime.now().millisecondsSinceEpoch.toString();
+
+    StorageReference storageReference =
+        FirebaseStorage.instance.ref().child(imageFileName);
+
+    StorageUploadTask storageUploadTask = storageReference.putFile(_imageFile);
+
+    StorageTaskSnapshot taskSnapshot = await storageUploadTask.onComplete;
+
+    await taskSnapshot.ref.getDownloadURL().then((urlImage) {
+      userImageUrl = urlImage;
+
+      _registerUser();
+    });
+  }
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  void _registerUser() async {
+    FirebaseUser firebaseUser;
+
+    await _auth
+        .createUserWithEmailAndPassword(
+      email: _emailTextEditingController.text.trim(),
+      password: _passwordTextEditingController.text.trim(),
+    )
+        .then((auth) {
+      firebaseUser = auth.user;
+    }).catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorAlertDialog(
+              message: error.message.toString(),
+            );
+          });
+    });
+
+    if (firebaseUser != null) {
+      saveUserInfoToFireStore(firebaseUser).then((value) {
+        Navigator.pop(context);
+        Route route = MaterialPageRoute(builder: (c) => StoreHome());
+        Navigator.pushReplacement(context, route);
+      });
+    }
+  }
+
+  Future saveUserInfoToFireStore(FirebaseUser fUser) async {
+    Firestore.instance.collection("users").document(fUser.uid).setData({
+      "uid": fUser.uid,
+      "email": fUser.email,
+      "name": _nameTextEditingController.text.trim(),
+      "url": userImageUrl,
+      EcommerceApp.userCartList: ["garbageValue"],
+    });
+
+    await EcommerceApp.sharedPreferences.setString("uid", fUser.uid);
+    await EcommerceApp.sharedPreferences
+        .setString(EcommerceApp.userEmail, fUser.email);
+    await EcommerceApp.sharedPreferences
+        .setString(EcommerceApp.userName, _nameTextEditingController.text);
+    await EcommerceApp.sharedPreferences
+        .setString(EcommerceApp.userAvatarUrl, userImageUrl);
+    await EcommerceApp.sharedPreferences
+        .setStringList(EcommerceApp.userCartList, ["garbageValue"]);
   }
 }
-
